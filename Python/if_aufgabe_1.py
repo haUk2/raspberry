150ed0002d8b05ff7004335f2aaea12ec0f3afa2
haUk2 <jakob.jochum@student.htldornbirn.at>
2022-10-24 09:01:26 +0200

    if_aufgabe_1

--- Python/if_aufgabe_1.py
+++ Python/if_aufgabe_1.py
@@ -2,14 +2,14 @@
 
 import random
 
-randomNumber = randrage.random(100)
+randomNumber = random.randrange(100)
 print(randomNumber)
 
 if randomNumber < 20:
     print("Mini")
-    elif randomNumber < 50
+elif randomNumber < 50:
     print("Medium")
-    elif randomNumber > 50
+elif randomNumber > 50:
     print("Large")
 
 
