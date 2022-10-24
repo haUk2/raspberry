47740ec85ac0aeea8f1fd9112d937939e6c1c1b1
haUk2 <jakob.jochum@student.htldornbirn.at>
2022-10-24 08:55:03 +0200

    first comit

---
+++ Python/if_aufgabe_1.py
@@ -0,0 +1,16 @@
+
+
+import random
+
+randomNumber = randrage.random(100)
+print(randomNumber)
+
+if randomNumber < 20:
+    print("Mini")
+    elif randomNumber < 50
+    print("Medium")
+    elif randomNumber > 50
+    print("Large")
+
+
+
